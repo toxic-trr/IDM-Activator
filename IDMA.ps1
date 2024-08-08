1520a7757568049c809ae20d61e769d054be5c26
Tanvir Hasan <63407839+toxic-trr@users.noreply.github.com>
2024-08-08 12:32:11 +0600

    Add files via upload

---
+++ IDMA.ps1
@@ -0,0 +1,19 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://toxic-trr.github.io/IDM-Activator/'
+
+$FilePath = "$env:TEMP\IDMA.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
